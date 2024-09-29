3993989ad5344fbd650d1e5dd485bd57d31c7d75
Gabriel Seltzer <gabeseltzer@gmail.com>
2024-09-29 19:23:58 -0400

    setMonitors not working

--- TVMode.ps1
+++ TVMode.ps1
@@ -36,10 +36,21 @@ $currentDefaultAudioDevice = Get-AudioDevice -Playback
 if ($currentDefaultAudioDevice.name -eq $headphoneDevice.name) {
   Write-Host "switching to speaker mode"
   Set-AudioDevice -ID $speakerDevice.ID
-  ./MultiMonitorTool/MultiMonitorTool.exe /enable \\.\DISPLAY2
-  # "C:\Program Files (x86)\Steam\steam.exe" "steam://open/bigpicture"  
+  
+  # Small off, LG off, TV on
+  # MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 Primary=1 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0" "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" 
+  
+  # Small off, LG on, TV on
+  ./MultiMonitorTool/MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 Primary=1 BitsPerPixel=32 Width=2560 Height=1440 DisplayFlags=0 DisplayFrequency=144 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0"
+  
+  # "C:\Program Files (x86)\Steam\steam.exe" "steam://open/bigpicture" 
 } elseif ($currentDefaultAudioDevice.name -eq $speakerDevice.name) {
   Write-Host "switching to headphone mode"
   Set-AudioDevice -ID $headphoneDevice.ID
-  ./MultiMonitorTool/MultiMonitorTool.exe /disable \\.\DISPLAY2
+  
+  # Small on, LG on, TV off
+  # MultiMonitorTool.exe /SetMonitors "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 Primary=1 BitsPerPixel=32 Width=2560 Height=1440 DisplayFlags=0 DisplayFrequency=144 DisplayOrientation=0 PositionX=0 PositionY=0"  "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0"
+  
+  # Small on, LG off, TV on
+  ./MultiMonitorTool/MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=32 Width=1920 Height=1080 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=4294965376 PositionY=240" "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0"
 }
