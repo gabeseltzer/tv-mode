a82d090a007077d214eb26d405e6438d2f894d8e
Gabriel Seltzer <gabeseltzer@gmail.com>
2024-12-02 19:10:22 -0500

    I think this worked but I want python

--- TVMode.ps1
+++ TVMode.ps1
@@ -40,8 +40,8 @@ if ($currentDefaultAudioDevice.name -eq $headphoneDevice.name) {
   # Small off, LG off, TV on
   # MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 Primary=1 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0" "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" 
   
-  # Small off, LG on, TV on
-  ./MultiMonitorTool/MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 Primary=1 BitsPerPixel=32 Width=2560 Height=1440 DisplayFlags=0 DisplayFrequency=144 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0"
+  # FOR TESTING Small off, LG on, TV on 
+  ./MultiMonitorTool/MultiMonitorTool.exe /LoadConfig .\small-off.cfg
   
   # "C:\Program Files (x86)\Steam\steam.exe" "steam://open/bigpicture" 
 } elseif ($currentDefaultAudioDevice.name -eq $speakerDevice.name) {
@@ -51,6 +51,6 @@ if ($currentDefaultAudioDevice.name -eq $headphoneDevice.name) {
   # Small on, LG on, TV off
   # MultiMonitorTool.exe /SetMonitors "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 Primary=1 BitsPerPixel=32 Width=2560 Height=1440 DisplayFlags=0 DisplayFrequency=144 DisplayOrientation=0 PositionX=0 PositionY=0"  "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0"
   
-  # Small on, LG off, TV on
-  ./MultiMonitorTool/MultiMonitorTool.exe /SetMonitors "Name=MONITOR\SAM0A59\{4d36e96e-e325-11ce-bfc1-08002be10318}\0007 BitsPerPixel=32 Width=1920 Height=1080 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=4294965376 PositionY=240" "Name=MONITOR\GSM5B7F\{4d36e96e-e325-11ce-bfc1-08002be10318}\0011 BitsPerPixel=0 Width=0 Height=0 DisplayFlags=0 DisplayFrequency=0 DisplayOrientation=0 PositionX=0 PositionY=0" "Name=MONITOR\SAM0D3B\{4d36e96e-e325-11ce-bfc1-08002be10318}\0005 BitsPerPixel=32 Width=3840 Height=2160 DisplayFlags=0 DisplayFrequency=60 DisplayOrientation=0 PositionX=2560 PositionY=0"
+  # FOR TESTING Small on, LG off, TV on
+  ./MultiMonitorTool/MultiMonitorTool.exe /LoadConfig .\lg-off.cfg
 }
